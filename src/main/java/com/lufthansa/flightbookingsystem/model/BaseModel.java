40fb1158242200c60e5f201ab6680f676c1bdd6f
Utsav <utsavrastogi@outlook.com>
2024-03-04 04:12:10 +0530

    added BaseModel with type-4 UUID generator

---
+++ src/main/java/com/lufthansa/flightbookingsystem/model/BaseModel.java
@@ -0,0 +1,22 @@
+package com.lufthansa.flightbookingsystem.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel {
+    @Id
+    @GeneratedValue(generator = "uuid-generator")
+    @GenericGenerator(name = "uuid-generator", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", nullable = false, updatable = false, columnDefinition = "binary(16)")
+    private UUID id; // type-4 UUID
+}
