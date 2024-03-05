986c173cd2b1e091f50bdb630be0888d4ab40fd9
Utsav <utsavrastogi@outlook.com>
2024-03-05 10:55:24 +0530

    adding functional integration test to returnListOfProductsWhenProductExists

--- src/main/java/com/lufthansa/flightbookingsystem/dto/UserResponseDto.java
+++ src/main/java/com/lufthansa/flightbookingsystem/dto/UserResponseDto.java
@@ -1,12 +1,14 @@
 package com.lufthansa.flightbookingsystem.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserResponseDto {
     private UUID id;
     private String email;
--- src/test/java/com/lufthansa/flightbookingsystem/controller/UserControllerWebMvcTest.java
+++ src/test/java/com/lufthansa/flightbookingsystem/controller/UserControllerWebMvcTest.java
@@ -1,16 +1,25 @@
 package com.lufthansa.flightbookingsystem.controller;
 
+import com.lufthansa.flightbookingsystem.dto.UserResponseDto;
 import com.lufthansa.flightbookingsystem.exception.UserNotFoundException;
 import com.lufthansa.flightbookingsystem.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest(UserController.class)
 public class UserControllerWebMvcTest {
@@ -26,5 +35,24 @@ public class UserControllerWebMvcTest {
         when(userService.findAllUsers()).thenThrow(UserNotFoundException.class);
         mockMvc.perform(get("/api/v1/users"))
                 .andExpect(status().is(404));
+    }
+
+    @Test
+    void returnListOfProductsWhenProductExists() throws Exception {
+        List<UserResponseDto> users = new ArrayList<>();
+        users.add(UserResponseDto.builder().id(UUID.randomUUID())
+                .email("abc@gmail.com")
+                .address("Ghaziabad")
+                .nationality("Indian")
+                .password("pwd123")
+                .phoneNumber("+918881886454")
+                .build());
+        when(userService.findAllUsers()).thenReturn(users);
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].email", is("abc@gmail.com")))
+                .andExpect(jsonPath("$[0].address", is("Ghaziabad")));
     }
 }
